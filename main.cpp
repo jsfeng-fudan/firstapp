07c6d4e3fe5cd651e48a26da7d4ce65000e1ac6f
Junsheng Feng <fjs@hfnu.edu.cn>
2022-03-20 21:56:30 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello,world! Fengjunsheng" << endl;
+    int i = 10;
+    cout << "i = " << i << endl;
+    if( i> 5)
+    {
+        cout << "larger than " << i << endl; 
+    }
+    else
+    {
+         cout << "lesser than " << i << endl;
+    }
+
+    for (int a = 10; a < 20; a = a + 1)
+    {
+        cout << "a µÄÖµ£º" << a << endl;
+    }
+
+    return 0;
+}
+
